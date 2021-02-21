95cf36af4f7e187275f0f0bb2e0fdbab22a27c23
Vovchanchyk <oberigfast@gmail.com>
2021-02-21 18:34:08 +0200

    last

--- components/wrapper.tsx
+++ components/wrapper.tsx
@@ -11,7 +11,7 @@ export function Wrapper({children}){
     const dispatch = useDispatch();
     const handle=()=>{
      dispatch(logOutActionCreator())
-     router.push('/')
+     router.push('/signup')
     }
     return(
         <div className={styles.wrapper}>
